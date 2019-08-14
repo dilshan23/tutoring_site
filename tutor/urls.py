dddc5badb12078563a23d462ddb48c4ad8be69e1
dilshan23 <38337550+dilshan23@users.noreply.github.com>
2019-08-14 11:18:29 +0530

    Update urls.py

--- tutor/urls.py
+++ tutor/urls.py
@@ -2,15 +2,20 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from . import views
 
+
+app_name = "tutor"
 urlpatterns = [
     path('',views.index,name = 'index'),
 
     path('login/',views.login_user,name = 'login_user' ),
     path('register/', views.register,name = 'register'),
     path("logout/",views.logout_user,name = 'logout'),
-     #path("quiz/",views.quiz,name = 'quiz'),
-     path("getting_answers/",views.getting_answers,name = 'getting_answers'),
-     path('quiz/<int:id>/',views.quiz , name='question-detail'),
+    #path("allquiz/",views.allquiz,name = 'allquiz'),
+    path("quiz/<int:question_id>/getting_answers/",views.getting_answers,name = 'getting_answers'),
+    path('quiz/<int:question_id>/',views.quiz , name='quiz'),
+
+
+    #path('book/<int:pk>/renew/', renew_book_librarian, name='renew-book-librarian'),
 
 
     
