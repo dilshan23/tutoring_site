fb6980a7f4afdc34ad2973f10dd6b75d4aa0d2fe
dilshan23 <38337550+dilshan23@users.noreply.github.com>
2021-05-18 08:31:26 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,10 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
-
-
 def login_user(request):
-
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -20,7 +17,6 @@ def login_user(request):
             return redirect('login')
     else:
         return render(request, 'login.html', {})
-
 
 def logout_user(request):
     logout(request)
